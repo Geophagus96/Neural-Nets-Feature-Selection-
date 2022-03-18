5313741ce73193a751b7ca32da47259db587e724
Yuze Zhou <33130813+Geophagus96@users.noreply.github.com>
2022-03-18 10:41:58 -0400

    Update Input_decay_regression.py

--- Input_decay_regression.py
+++ Input_decay_regression.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 16 10:54:45 2022
-
-@author: Yuze Zhou
-"""
-
 import torch 
 import time
 from torch.autograd import Variable
@@ -15,11 +8,15 @@ from sklearn.linear_model import LinearRegression
 class MLP(torch.nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
-        self.linear1 = torch.nn.Linear(800, 1)
+        self.linear1 = torch.nn.Linear(800, 600)
+        self.linear2 = torch.nn.Linear(600, 300)
+        self.linear3 = torch.nn.Linear(300,1)
 
     def forward(self, x):
-        layer1_out = self.linear1(x)
-        return layer1_out
+        layer1_out = F.tanh(self.linear1(x))
+        layer2_out = F.tanh(self.linear2(layer1_out))
+        out = F.tanh(self.linear3(layer2_out))
+        return out
 
 def input_decay(weights, eta):
     neuron_l2 = torch.sum(torch.pow(weights, 2), axis=0)
@@ -30,12 +27,12 @@ start = time.time()
 train_size = 1000
 dim_size = 400
 valid_size = 500
-beta = torch.cat((torch.rand(dim_size), torch.zeros(dim_size)), dim=0)
-beta = beta.reshape((2*dim_size,1))
+#beta = torch.cat((torch.rand(dim_size), torch.zeros(dim_size)), dim=0)
+#beta = beta.reshape((2*dim_size,1))
 x_train = torch.rand((train_size, 2*dim_size))
 x_valid = torch.rand((valid_size, 2*dim_size))
-y_train = torch.mm(x_train, beta)+torch.rand((train_size,1))
-y_valid = torch.mm(x_valid, beta)+torch.rand((valid_size,1))
+y_train = torch.rand(train_size)
+y_valid = torch.rand(valid_size)
 
 device = torch.device('cuda:0')
 inputs = Variable(x_train).to(device)
@@ -44,17 +41,19 @@ inputs_valid = Variable(x_valid).to(device)
 targets_valid = Variable(y_valid).to(device)
 
 model = MLP().to(device)
-optimizer = torch.optim.SGD(model.parameters(), lr=2e-3)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
-phi, eta = 0.1, 0.1
+phi, eta = 0.001, 0.01
 
-num_epoch = 21000
+num_epoch = 18000
 for i in range(num_epoch):
     outputs = model(inputs)
-    optimizer.zero_grad()
+    #optimizer.zero_grad()
     input_regularizer = input_decay(model.linear1.weight, eta) 
     mse_loss = F.mse_loss(outputs, targets)
     loss =  mse_loss + phi*input_regularizer 
+    if (i%300==1):
+        print(mse_loss)
     loss.backward() 
     optimizer.step()
     
@@ -63,6 +62,6 @@ print(lag)
 print(F.mse_loss(model(inputs_valid),targets_valid))
 
 reg = LinearRegression().fit(np.array(x_train), np.array(y_train))
-y_predict = reg.predict(x_valid)
+y_predict = reg.predict(np.array(x_valid))
 print(F.mse_loss(torch.tensor(y_predict), y_valid))
 
